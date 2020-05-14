ef4b17c1d63538eca0215f37caa5ccfb666dba1d
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-05-14 21:28:12 +0200

    Remove an operator incompatible with c++17/20

--- src/3rdparty/glslang/glslang/Include/PoolAlloc.h
+++ src/3rdparty/glslang/glslang/Include/PoolAlloc.h
@@ -241,7 +241,6 @@ protected:
     int numCalls;           // just an interesting statistic
     size_t totalBytes;      // just an interesting statistic
 private:
-    TPoolAllocator& operator=(const TPoolAllocator&);  // don't allow assignment operator
     TPoolAllocator(const TPoolAllocator&);  // don't allow default copy constructor
 };
 
